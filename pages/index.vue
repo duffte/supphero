c993cd068f6b211262a368c755ee49e160cc6dcb
duffte <dufftee@gmail.com>
2018-11-28 12:10:52 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -20,10 +20,45 @@
 <script>
 import { fireDb } from '~/plugins/firebase.js'
 export default {
+  head() {
+    return {
+      title: this.description,
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.description
+        },
+        //facebook
+        { property: 'og:title', content: this.title },
+        { property: 'og:description', content: this.description },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:image', content: this.image },
+        //twitter
+        { name: 'twitter:card', content: 'summary' },
+        { name: 'twitter:site', content: '@supphero' },
+        { name: 'twitter:title', content: this.title },
+        {
+          name: 'twitter:description',
+          content: this.description
+        },
+        {
+          name: 'twitter:image',
+          content: this.image
+        },
+        { name: 'twitter:image:alt', content: this.title }
+      ]
+    }
+  },
   name: 'HomePage',
   data() {
     return {
-      symptome: []
+      symptome: [],
+      title: 'Supphero - Das passende Supplement für jedes Symptom',
+      description:
+        'Supphero erlaubt einfache Einsicht in die Wirkungsweisen von Supplementen bei typischen Symptomen.',
+      image: 'https://supphero.netlify.com/_nuxt/img/368164f.svg'
     }
   },
   async asyncData() {
