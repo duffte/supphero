1bf0779ba6ad794a39084f9d3d28f8fc8140cdbf
duffte <dufftee@gmail.com>
2018-12-15 11:28:53 +0100

    index

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,19 @@
 <template>
   <section class="section">
-    <div class="columns is-mobile">
-      <div class="column title-left">
-        <p class="title">Welches Supplement hilft bei</p>
-      </div>
-      <div class="column title-center has-text-left">
-        <ul>
-          <li v-for="symptom in symptome" :key="symptom.id">
-            <nuxt-link :to="'/symptom/'+symptom.id">
-              <p class="title has-text-primary">{{ symptom.symptomName }}</p>
-            </nuxt-link>
-          </li>
-        </ul>
+    <div class="container">
+      <div class="columns is-mobile">
+        <div class="column has-text-right">
+          <p class="title">Welches Supplement hilft bei</p>
+        </div>
+        <div class="column title-center has-text-left">
+          <ul>
+            <li v-for="symptom in symptome" :key="symptom.id">
+              <nuxt-link :to="'/symptom/'+symptom.id">
+                <p class="title has-text-primary">{{ symptom.symptomName }}</p>
+              </nuxt-link>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </section>
