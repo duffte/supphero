2bc6fe994100d12fe444f2cd19fb1769388c74ce
duffte <dufftee@gmail.com>
2019-01-18 00:35:48 +0100

    Update NavBar.vue

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <template>
   <nav id="nav" class="navbar is-primary" role="navigation" aria-label="main navigation">
     <div class="container">
@@ -50,48 +49,3 @@ export default {
 }
 </style>
 
-=======
-<template>
-  <nav id="nav" class="navbar is-primary" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <router-link to="/" class="navbar-item">
-        <img src="~assets/supphero.svg" width="112" height="28">
-      </router-link>
-      <a
-        :class="{ 'is-active': showNav }"
-        role="button"
-        class="navbar-burger burger"
-        @click="showNav = !showNav"
-      >
-        <span aria-hidden="true"/>
-        <span aria-hidden="true"/>
-        <span aria-hidden="true"/>
-      </a>
-    </div>
-    <div id="navbarToggle" :class="{ 'is-active': showNav }" class="navbar-menu">
-      <div class="navbar-start">
-        <nuxt-link class="navbar-item" to="/symptom">Symptome</nuxt-link>
-        <nuxt-link class="navbar-item" to="/wirkstoff">Wirkstoffe</nuxt-link>
-        <nuxt-link class="navbar-item" to="/artikel">Lesen</nuxt-link>
-      </div>
-    </div>
-  </nav>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      showNav: false
-    }
-  }
-}
-</script>
-
-<style>
-.navbar.is-primary {
-  background-color: #694be3 !important;
-}
-</style>
-
->>>>>>> 4d922a0639d42ada25c7c37200b4bf6d90cbc2f0
