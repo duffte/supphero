04a1b8e8344a51f5e332f150e01c42321ca37cff
duffte <dufftee@gmail.com>
2019-01-18 06:31:06 +0100

    Update NavBar.vue

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -26,7 +26,7 @@
           <nuxt-link class="navbar-item" to="/artikel" title="Lesen">Lesen</nuxt-link>
           <nuxt-link class="navbar-item" to="/stack">
               <span v-if="cartTotal > 0" class="badge is-badge-success">{{cartTotal}}</span>
-              <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+              <svg style="margin-left:.25em;width:24px;height:24px" viewBox="0 0 24 24">
                 <path fill="#ffffff" d="M12,21.35L10.55,20.03C5.4,15.36 2,12.27 2,8.5C2,5.41 4.42,3 7.5,3C9.24,3 10.91,3.81 12,5.08C13.09,3.81 14.76,3 16.5,3C19.58,3 22,5.41 22,8.5C22,12.27 18.6,15.36 13.45,20.03L12,21.35Z" />
               </svg>
           </nuxt-link>
