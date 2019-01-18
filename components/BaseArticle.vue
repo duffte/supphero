dfb7f9a45948810a955faf52d882c2e554308a2b
duffte <dufftee@gmail.com>
2019-01-18 06:47:07 +0100

    fix icons

--- components/BaseArticle.vue
+++ components/BaseArticle.vue
@@ -11,9 +11,13 @@
         </nuxt-link>
         <div class="card-content">
           <p class="subtitle is-6 has-text-grey">
-            icon
+            <svg style="width:12px;height:12px" viewBox="0 0 24 24">
+              <path fill="#000000" d="M9,10H7V12H9V10M13,10H11V12H13V10M17,10H15V12H17V10M19,3H18V1H16V3H8V1H6V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5A2,2 0 0,0 19,3M19,19H5V8H19V19Z" />
+          </svg>
             {{ thedate }}
-            icon
+            <svg style="width:12px;height:12px" viewBox="0 0 24 24">
+              <path fill="#000000" d="M12,20A7,7 0 0,1 5,13A7,7 0 0,1 12,6A7,7 0 0,1 19,13A7,7 0 0,1 12,20M19.03,7.39L20.45,5.97C20,5.46 19.55,5 19.04,4.56L17.62,6C16.07,4.74 14.12,4 12,4A9,9 0 0,0 3,13A9,9 0 0,0 12,22C17,22 21,17.97 21,13C21,10.88 20.26,8.93 19.03,7.39M11,14H13V8H11M15,1H9V3H15V1Z" />
+          </svg>
             {{ timeToRead }} min Lesezeit
           </p>
           <p class="title is-4 newsTitle">
--- pages/artikel/_id.vue
+++ pages/artikel/_id.vue
@@ -13,7 +13,14 @@
                   <p class="subtitle">
                     {{ data.data.artikelExcerpt }}
                   </p>
-                  <span>icon {{ theDate }} icon Lesedauer: {{ data.data.timeToRead }} Minute(n)</span>
+                  <span>
+                    <svg style="width:12px;height:12px" viewBox="0 0 24 24">
+                        <path fill="#000000" d="M9,10H7V12H9V10M13,10H11V12H13V10M17,10H15V12H17V10M19,3H18V1H16V3H8V1H6V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5A2,2 0 0,0 19,3M19,19H5V8H19V19Z" />
+                    </svg>
+                      {{ theDate }}
+                      <svg style="width:12px;height:12px" viewBox="0 0 24 24">
+                        <path fill="#000000" d="M12,20A7,7 0 0,1 5,13A7,7 0 0,1 12,6A7,7 0 0,1 19,13A7,7 0 0,1 12,20M19.03,7.39L20.45,5.97C20,5.46 19.55,5 19.04,4.56L17.62,6C16.07,4.74 14.12,4 12,4A9,9 0 0,0 3,13A9,9 0 0,0 12,22C17,22 21,17.97 21,13C21,10.88 20.26,8.93 19.03,7.39M11,14H13V8H11M15,1H9V3H15V1Z" />
+                    </svg> Lesedauer: {{ data.data.timeToRead }} Minute(n)</span>
                   <hr>
                   <nuxt-link to="/autoren/" title="Autoren">
                     <div class="author media">
